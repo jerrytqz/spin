31cf17ef07d99b5c358a3cff503254fbefbd7418
JerryZhengPro <jerryzhengpro@gmail.com>
2022-09-10 16:44:44 -0400

    Fix profile card display

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -5,13 +5,15 @@ import { numberWithCommas } from '../../shared/utility';
 import { Link } from 'react-router-dom';
 
 const ProfileCard = (props) => (
-    <Link to={'/profile/' + props.user} className={classes.ProfileCard}>
-        <img src={logoImg} alt="Spin"/>
-        <div className={classes.Text}>
-            <div className={classes.User}>{props.user}</div>
-            <div className={classes.SP}>{numberWithCommas(props.sp)} SP</div>
-        </div>
-    </Link>
+    <div>
+        <Link to={'/profile/' + props.user} className={classes.ProfileCard}>
+            <img src={logoImg} alt="Spin"/>
+            <div className={classes.Text}>
+                <div className={classes.User}>{props.user}</div>
+                <div className={classes.SP}>{numberWithCommas(props.sp)} SP</div>
+            </div>
+        </Link>
+    </div>
 );
 
 export default ProfileCard; 
